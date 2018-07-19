78ce621bd1dcde7cbd14590183b39c7ce450bc61
Omkar1493 <omkar.r.bapat@gmail.com>
2018-07-19 19:42:45 -0400

    First commit

--- src/reactify-ui/src/App.js
+++ src/reactify-ui/src/App.js
@@ -1,21 +1,48 @@
-import React, { Component } from 'react';
+/*import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Posts from './posts/Posts';
+import { BrowserRouter, Switch, Redirect, Route } from 'react-router-dom'
+import PostDetail from './posts/PostDetail'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+      <Switch>
+        <Route exact path = '/posts' component={Posts} />
+        <Route exact path = '/posts/:slug' component={PostDetail} />        
+        <Route component={Posts} />
+      </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+<Route exact path = '/posts/:slug' component={Posts} />
+        
+export default App;*/
+
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch} from 'react-router-dom'
+import logo from './logo.svg';
+import './App.css';
+
+import Posts from './posts/Posts';
+import PostDetail from './posts/PostDetail';
+import PostCreate from './posts/PostCreate'
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+          <Switch>
+            <Route exact path='/posts/create' component={PostCreate}/>
+            <Route exact path='/posts' component={Posts}/>
+            <Route exact path='/posts/:slug' component={PostDetail}/>
+            <Route component={Posts}/>
+          </Switch>
+      </BrowserRouter>
     );
   }
 }
 
 export default App;
+
--- src/reactify/urls.py
+++ src/reactify/urls.py
@@ -13,13 +13,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+'''from django.contrib import admin
 from django.views.generic import TemplateView
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='react.html')),
+    re_path(r'^posts/', TemplateView.as_view(template_name='react.html')),
+    path('admin/', admin.site.urls),
+    path('api/posts/', include('posts.urls'))
+]
+'''
+from django.contrib import admin
+from django.views.generic import TemplateView
+from django.urls import path, include, re_path
+
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='react.html')),
+    path('walking', TemplateView.as_view(template_name='walking.html')),
+    re_path(r'^posts/', TemplateView.as_view(template_name='react.html')),
     path('admin/', admin.site.urls),
     path('api/posts/', include('posts.urls'))
 ]
